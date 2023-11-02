29b63b598b052122f890dd7d20033fedd6451293
unknown <email@example.com>
2023-11-02 15:00:27 -0600

    adding build rules and built a newer version

--- build.js
+++ build.js
@@ -3,9 +3,9 @@ var electronInstaller = require('electron-winstaller');
 // In this case, we can use relative paths
 var settings = {
     // Specify the folder where the built app is located
-    appDirectory: './EvoPhoneConsole-win32-x64',
+    appDirectory: './',
     // Specify the existing folder where 
-    outputDirectory: '../EvoPhoneConsoleInstaller',
+    outputDirectory: '../EvoPhoneConsole-win32-x64',
     // The name of the Author of the app (the name of your company)
     authors: 'TouchstoneIQ',
     // The name of the executable of your built
